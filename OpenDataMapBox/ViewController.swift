44d41f480b7e0632c87c393599db08fad64451e7
gupuru <kouhei.aquamarines@gmail.com>
2016-06-06 19:11:39 +0900

    first commit

---
+++ OpenDataMapBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenDataMapBox
+//
+//  Created by 新見晃平 on 2016/06/06.
+//  Copyright © 2016年 kohei Niimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
